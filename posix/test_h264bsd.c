5e5feb8fc8da3889301dabe105f7680488c99caf
Sam Leitch <sam@luceva.net>
2017-07-11 11:31:11 -0400

    Updated posix to include includes and optionally output files on test

--- posix/test_h264bsd.c
+++ posix/test_h264bsd.c
@@ -11,6 +11,9 @@
 #include "../src/h264bsd_decoder.h"
 #include "../src/h264bsd_util.h"
 
+static char* outputDir = NULL;
+static int repeatTest = 0;
+
 void createContentBuffer(char* contentPath, u8** pContentBuffer, size_t* pContentSize) {
   struct stat sb;
   if (stat(contentPath, &sb) == -1) {
@@ -23,18 +26,39 @@ void createContentBuffer(char* contentPath, u8** pContentBuffer, size_t* pConten
 }
 
 void loadContent(char* contentPath, u8* contentBuffer, size_t contentSize) {
-  int fd = open(contentPath, O_RDONLY);
-  if (fd == -1) {
+  FILE *input = fopen(contentPath, "r");
+  if (input == NULL) {
     perror("open failed");
     exit(1);
   }
 
   off_t offset = 0;
   while(offset < contentSize) {
-    offset += read(fd, contentBuffer + offset, contentSize - offset);
+    offset += fread(contentBuffer + offset, sizeof(u8), contentSize - offset, input);
   }
 
-  close(fd);
+  fclose(input);
+}
+
+void saveImage(int picNum, u8* picData, int width, int height) {
+  if(!outputDir) return;
+
+  char outputPath[128];
+  sprintf(outputPath, "%s/frame_%02d.yuv", outputDir, picNum);
+
+  FILE *output = fopen(outputPath, "w");
+  if (output == NULL) {
+    perror("open failed");
+    exit(1);
+  }
+
+  size_t picSize = width * height * 3 / 2;
+  off_t offset = 0;
+  while(offset < picSize) {
+    offset += fwrite(picData + offset, sizeof(u8), picSize - offset, output);
+  }
+
+  fclose(output);
 }
 
 void decodeContent(u8* contentBuffer, size_t contentSize) {
@@ -64,6 +88,7 @@ void decodeContent(u8* contentBuffer, size_t contentSize) {
       case H264BSD_PIC_RDY:
         pic = h264bsdNextOutputPicture(&dec, &picId, &isIdrPic, &numErrMbs);
         ++numPics;
+        saveImage(numPics, pic, width, height);
         break;
       case H264BSD_HDRS_RDY:
         h264bsdCroppingParams(&dec, &croppingFlag, &left, &width, &top, &height);
@@ -91,17 +116,37 @@ void decodeContent(u8* contentBuffer, size_t contentSize) {
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+  int c = getopt (argc, argv, "ro:");
+  while (c != -1) {
+    switch (c) {
+      case 'o':
+        outputDir = optarg;
+        break;
+      case 'r':
+        repeatTest = 1;
+        break;
+      default:
+        abort ();
+    }
+    c = getopt (argc, argv, "o");
+  }
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s -r [-o <outputDir>] <test_video.h264>\n", argv[0]);
     exit(1);
   }
 
-  char *contentPath = argv[1];
+  char *contentPath = argv[argc - 1];
   u8* contentBuffer;
   size_t contentSize;
   createContentBuffer(contentPath, &contentBuffer, &contentSize);
 
-  for(int i=0; i<10; ++i) {
+  if (repeatTest) {
+    while(1) {
+      loadContent(contentPath, contentBuffer, contentSize);
+      decodeContent(contentBuffer, contentSize);
+    }
+  } else {
     loadContent(contentPath, contentBuffer, contentSize);
     decodeContent(contentBuffer, contentSize);
   }
