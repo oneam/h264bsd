d845fb5e4d7e03cd55f0c627f5c5d81c4599ec8b
Sam Leitch <conceptualalchemist@gmail.com>
2017-07-11 21:30:01 -0400

    Updated test Rakefile so that it can build all test data (it requires ffmpeg to work). Added posix comparison test that compares output to test data. The comparison test currently fails, so I need to find out why.

--- posix/test_h264bsd.c
+++ posix/test_h264bsd.c
@@ -12,6 +12,7 @@
 #include "../src/h264bsd_util.h"
 
 static char* outputDir = NULL;
+static char* compareDir = NULL;
 static int repeatTest = 0;
 
 void createContentBuffer(char* contentPath, u8** pContentBuffer, size_t* pContentSize) {
@@ -33,20 +34,18 @@ void loadContent(char* contentPath, u8* contentBuffer, size_t contentSize) {
   }
 
   off_t offset = 0;
-  while(offset < contentSize) {
+  while (offset < contentSize) {
     offset += fread(contentBuffer + offset, sizeof(u8), contentSize - offset, input);
   }
 
   fclose(input);
 }
 
-void saveImage(int picNum, u8* picData, int width, int height) {
-  if(!outputDir) return;
+void saveImage(u8* picData, int width, int height, int picNum) {
+  char picPath[128];
+  sprintf(picPath, "%s/frame_%02d.yuv", outputDir, picNum);
 
-  char outputPath[128];
-  sprintf(outputPath, "%s/frame_%02d.yuv", outputDir, picNum);
-
-  FILE *output = fopen(outputPath, "w");
+  FILE *output = fopen(picPath, "w");
   if (output == NULL) {
     perror("open failed");
     exit(1);
@@ -54,19 +53,86 @@ void saveImage(int picNum, u8* picData, int width, int height) {
 
   size_t picSize = width * height * 3 / 2;
   off_t offset = 0;
-  while(offset < picSize) {
+  while (offset < picSize) {
     offset += fwrite(picData + offset, sizeof(u8), picSize - offset, output);
   }
 
   fclose(output);
 }
 
-void decodeContent(u8* contentBuffer, size_t contentSize) {
+static u8* expectedData = NULL;
+static int totalErrors = 0;
+
+int compareImage(u8* actualData, int width, int height, int picNum) {
+  char expectedPath[128];
+  sprintf(expectedPath, "%s/frame_%02d.yuv", compareDir, picNum);
+
+  FILE *input = fopen(expectedPath, "r");
+  if (input == NULL) {
+    perror("open failed");
+    exit(1);
+  }
+
+  size_t picSize = width * height * 3 / 2;
+  size_t uDataOffset = width * height;
+  size_t vDataOffset = width * height * 5 / 4;
+
+  if (!expectedData) expectedData = (u8*)malloc(picSize);
+
+  off_t offset = 0;
+  while (offset < picSize) {
+    offset += fread(expectedData + offset, sizeof(u8), picSize - offset, input);
+  }
+
+  fclose(input);
+
+  int numErrors = 0;
+
+  size_t yOffset = 0;
+  size_t uvOffset = 0;
+
+  u8* yExpected = expectedData;
+  u8* uExpected = expectedData + uDataOffset;
+  u8* vExpected = expectedData + vDataOffset;
+
+  u8* yActual = actualData;
+  u8* uActual = actualData + uDataOffset;
+  u8* vActual = actualData + vDataOffset;
+
+  for (int y=0; y<height; ++y) {
+    for (int x=0; x<width; ++x) {
+      int ySame = yActual[yOffset] == yExpected[yOffset];
+      int uSame = uActual[uvOffset] == uExpected[uvOffset];
+      int vSame = vActual[uvOffset] == vExpected[uvOffset];
+
+      if(!ySame || !uSame || !vSame) {
+        ++numErrors;
+        if (numErrors <= 5) {
+          printf(
+            "Pixel (%d,%d) is different. Expected (%d,%d,%d) but saw (%d,%d,%d).\n",
+            x, y,
+            yExpected[yOffset], uExpected[uvOffset], vExpected[uvOffset],
+            yActual[yOffset], uActual[uvOffset], vActual[uvOffset]);
+        }
+
+        if (numErrors == 6) printf("...\n");
+      }
+
+      ++yOffset;
+      if(yOffset % 1) ++ uvOffset;
+    }
+  }
+
+  if(numErrors > 0) printf("%d pixels are different on frame %d.\n\n", numErrors, picNum);
+  return numErrors;
+}
+
+void decodeContent (u8* contentBuffer, size_t contentSize) {
   u32 status;
   storage_t dec;
   status = h264bsdInit(&dec, HANTRO_FALSE);
 
-  if(status != HANTRO_OK) {
+  if (status != HANTRO_OK) {
     fprintf(stderr, "h264bsdInit failed\n");
     exit(1);
   }
@@ -78,21 +144,23 @@ void decodeContent(u8* contentBuffer, size_t contentSize) {
   u8* pic;
   u32 picId, isIdrPic, numErrMbs;
   u32 top, left, width, height, croppingFlag;
+  int totalErrors = 0;
 
-  while(len > 0) {
+  while (len > 0) {
     u32 result = h264bsdDecode(&dec, byteStrm, len, 0, &readBytes);
     len -= readBytes;
     byteStrm += readBytes;
 
-    switch(result) {
+    switch (result) {
       case H264BSD_PIC_RDY:
         pic = h264bsdNextOutputPicture(&dec, &picId, &isIdrPic, &numErrMbs);
         ++numPics;
-        saveImage(numPics, pic, width, height);
+        if (outputDir) saveImage(pic, width, height, numPics);
+        if (compareDir) totalErrors += compareImage(pic, width, height, numPics);
         break;
       case H264BSD_HDRS_RDY:
         h264bsdCroppingParams(&dec, &croppingFlag, &left, &width, &top, &height);
-        if(!croppingFlag) {
+        if (!croppingFlag) {
           width = h264bsdPicWidth(&dec) * 16;
           height = h264bsdPicHeight(&dec) * 16;
         }
@@ -111,28 +179,32 @@ void decodeContent(u8* contentBuffer, size_t contentSize) {
     }
   }
 
-  printf("Test file complete. %d pictures decoded.\n", numPics);
   h264bsdShutdown(&dec);
+
+  printf("Test file complete. %d pictures decoded.\n", numPics);
+  if (compareDir) printf("%d errors found.\n", totalErrors);
 }
 
 int main(int argc, char *argv[]) {
-  int c = getopt (argc, argv, "ro:");
-  while (c != -1) {
+  int c;
+  while ((c = getopt (argc, argv, "ro:c:")) != -1) {
     switch (c) {
       case 'o':
         outputDir = optarg;
+        break;
+      case 'c':
+        compareDir = optarg;
         break;
       case 'r':
         repeatTest = 1;
         break;
       default:
-        abort ();
+        abort();
     }
-    c = getopt (argc, argv, "o");
   }
 
   if (argc < 2) {
-    fprintf(stderr, "Usage: %s -r [-o <outputDir>] <test_video.h264>\n", argv[0]);
+    fprintf(stderr, "Usage: %s [-r] [-c <actualDir>] [-o <outputDir>] <test_video.h264>\n", argv[0]);
     exit(1);
   }
 
@@ -142,7 +214,7 @@ int main(int argc, char *argv[]) {
   createContentBuffer(contentPath, &contentBuffer, &contentSize);
 
   if (repeatTest) {
-    while(1) {
+    while (1) {
       loadContent(contentPath, contentBuffer, contentSize);
       decodeContent(contentBuffer, contentSize);
     }
